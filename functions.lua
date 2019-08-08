a244ac56e115d721890b12a18c7f13c7fe994126
Ufuk Seyithan Erdem <ufukerdem_1999@hotmail.com>
2019-08-08 09:31:47 +0300

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -1,6 +1,6 @@
-gui.funcs = {}
+ui.funcs = {}
 
-function gui.funcs.deepcopy(object)
+function ui.funcs.deepcopy(object)
 	local lookup_table = {}
 	local function _copy(object)
 		if type(object) ~= "table" then
@@ -18,85 +18,85 @@ function gui.funcs.deepcopy(object)
 	return _copy(object)
 end
 
-function gui.funcs.info(text)
-    if not gui.config.showConsoleInputs then return end
+function ui.funcs.info(text)
+    if not ui.config.showConsoleInputs then return end
 
     print("\169128255255[INFO]: "..text)
 end
 
-function gui.funcs.error(text)
-    if not gui.config.showConsoleInputs then return end
+function ui.funcs.error(text)
+    if not ui.config.showConsoleInputs then return end
 
     print("\169255000128[ERROR]: "..text)
 end
 
-function gui.funcs.convertX(id, x)
+function ui.funcs.convertX(id, x)
     return (x / 850) * player(id, "screenw")
 end
 
-function gui.funcs.convertY(id, y)
+function ui.funcs.convertY(id, y)
     return (y / 480) * player(id, "screenh")
 end
 
-function gui.funcs.getMouseX(id)
+function ui.funcs.getMouseX(id)
     local x
     
     if player(id, "health") <= 0 then
         reqcld(id, 0)
 
-        x = gui.user.list[id].mouseX
+        x = ui.user.list[id].mouseX
     else
         x = player(id, "mousex")
     end
 
-    return gui.funcs.convertX(id, x)
+    return ui.funcs.convertX(id, x)
 end
 
-function gui.funcs.getMouseY(id)
+function ui.funcs.getMouseY(id)
     local y
     
     if player(id, "health") <= 0 then
         reqcld(id, 0)
 
-        y = gui.user.list[id].mouseY
+        y = ui.user.list[id].mouseY
     else
         y = player(id, "mousey")
     end
 
-    return gui.funcs.convertY(id, y)
+    return ui.funcs.convertY(id, y)
 end
 
-function gui.funcs.isInside(x, y, x1, y1, x2, y2)
+function ui.funcs.isInside(x, y, x1, y1, x2, y2)
     return ((x > x1) and (y > y1) and (x < x2) and (y < y2))
 end
 
-function gui.funcs.checkHoveredExists(id)
-    for k, v in pairs(gui.user.list[id].objects["buttons"]) do
+function ui.funcs.checkHoveredExists(id)
+    for k, v in pairs(ui.user.list[id].objects["buttons"]) do
         if v.hovered then
             return true
         end
     end
 
-    for k, v in pairs(gui.user.list[id].objects["imageButtons"]) do
+    for k, v in pairs(ui.user.list[id].objects["imageButtons"]) do
         if v.hovered then
             return true
         end
     end
 end
 
-function gui.funcs.click(id)
-    local x, y = gui.funcs.getMouseX(id), gui.funcs.getMouseY(id)
+function ui.funcs.click(id)
+    local x, y = ui.funcs.getMouseX(id), ui.funcs.getMouseY(id)
 
-    for k, v in pairs(gui.user.list[id].objects["buttons"]) do
-        if gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+    for k, v in pairs(ui.user.list[id].objects["buttons"]) do
+        if ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
             if v.onClick then
                 v.onClick(id, v)
             end
         end
     end
 
-    for k, v in pairs(gui.user.list[id].objects["imageButtons"]) do
-        if gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+    for k, v in pairs(ui.user.list[id].objects["imageButtons"]) do
+        if ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
             if v.onClick then
                 v.onClick(id, v)
             end
@@ -104,19 +104,19 @@ function gui.funcs.click(id)
     end
 end
 
-function gui.funcs.rightClick(id)
-    local x, y = gui.funcs.getMouseX(id), gui.funcs.getMouseY(id)
+function ui.funcs.rightClick(id)
+    local x, y = ui.funcs.getMouseX(id), ui.funcs.getMouseY(id)
 
-    for k, v in pairs(gui.user.list[id].objects["buttons"]) do
-        if gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+    for k, v in pairs(ui.user.list[id].objects["buttons"]) do
+        if ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
             if v.onRightClick then
                 v.onRightClick(id, v)
             end
         end
     end
 
-    for k, v in pairs(gui.user.list[id].objects["imageButtons"]) do
-        if gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+    for k, v in pairs(ui.user.list[id].objects["imageButtons"]) do
+        if ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
             if v.onRightClick then
                 v.onRightClick(id, v)
             end
@@ -124,15 +124,15 @@ function gui.funcs.rightClick(id)
     end
 end
 
-function gui.funcs.hover()
+function ui.funcs.hover()
     for _, i in pairs(player(0, "table")) do
-        local x, y = gui.funcs.getMouseX(i), gui.funcs.getMouseY(i)
+        local x, y = ui.funcs.getMouseX(i), ui.funcs.getMouseY(i)
 
-        if #gui.user.list[i].objects["buttons"] > 0 then
-            for k, v in pairs(gui.user.list[i].objects["buttons"]) do
-                if not gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+        if #ui.user.list[i].objects["buttons"] > 0 then
+            for k, v in pairs(ui.user.list[i].objects["buttons"]) do
+                if not ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
                     if v.onUnhover then
-                        if gui.config.limitedHovering then
+                        if ui.config.limitedHovering then
                             if v.hovered then
                                 v.onUnhover(i, v)
 
@@ -145,12 +145,12 @@ function gui.funcs.hover()
                 end
             end
 
-            for k, v in pairs(gui.user.list[i].objects["buttons"]) do
-                if gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+            for k, v in pairs(ui.user.list[i].objects["buttons"]) do
+                if ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
                     if v.onHover and not v.hovered then
                         v.onHover(i, v)
 
-                        if gui.config.limitedHovering then
+                        if ui.config.limitedHovering then
                             v.hovered = true
                         end
                     end
@@ -158,11 +158,11 @@ function gui.funcs.hover()
             end
         end
 
-        if #gui.user.list[i].objects["imageButtons"] > 0 then
-            for k, v in pairs(gui.user.list[i].objects["imageButtons"]) do
-                if not gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+        if #ui.user.list[i].objects["imageButtons"] > 0 then
+            for k, v in pairs(ui.user.list[i].objects["imageButtons"]) do
+                if not ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
                     if v.onUnhover then
-                        if gui.config.limitedHovering then
+                        if ui.config.limitedHovering then
                             if v.hovered then
                                 v.onUnhover(i, v)
 
@@ -175,12 +175,12 @@ function gui.funcs.hover()
                 end
             end
 
-            for k, v in pairs(gui.user.list[i].objects["imageButtons"]) do
-                if gui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
+            for k, v in pairs(ui.user.list[i].objects["imageButtons"]) do
+                if ui.funcs.isInside(x, y, v.x - v.width / 2, v.y - v.height / 2, v.x + v.width / 2, v.y + v.height / 2) then
                     if v.onHover and not v.hovered then
                         v.onHover(i, v)
 
-                        if gui.config.limitedHovering then
+                        if ui.config.limitedHovering then
                             v.hovered = true
                         end
                     end
@@ -189,4 +189,4 @@ function gui.funcs.hover()
         end
     end
 end
-timer(gui.config.mouseRefreshRate, "gui.funcs.hover", "", 0)
+timer(ui.config.mouseRefreshRate, "ui.funcs.hover", "", 0)
